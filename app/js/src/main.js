bfb554a5038934e798efbfac6626a0caba7d02ef
Kuznetsov Pavel <colorOfMagic@mail.ru>
2023-01-23 14:40:41 +0500

    adding blocks

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,32 +1,35 @@
-const body  = document.querySelector('body'),
+const body = document.querySelector('body'),
     burgerBtn = document.querySelector('.btn_burger'),
     closeBtn = document.querySelector('.btn_close'),
     mobileMenu = document.querySelector('.mobile_menu'),
     categoryListItems = document.querySelectorAll('.category_list_item'),
     toTopBtn = document.querySelector('[data-role="toTop"]'),
-    indicator = document.querySelector('.indicator');
+    indicator = document.querySelector('.indicator'),
+    favoriteBtns = document.querySelectorAll('.content .card button'),
+    inFavoriteBtn = document.querySelector('[data-role="inFavorite"]');
 // mobile menu
 
-    const closeMenu = ()=> {
-      mobileMenu.classList.remove('mobile_menu--active')
-      body.classList.remove('body--hidden')
-    }
+// console.log(inFavoriteBtn);
 
+const closeMenu = () => {
+    mobileMenu.classList.remove('mobile_menu--active');
+    body.classList.remove('body--hidden');
+};
 
-    burgerBtn.addEventListener('click', () => {
-      mobileMenu.classList.add('mobile_menu--active')
-      body.classList.add('body--hidden')
-    })
+burgerBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('mobile_menu--active');
+    body.classList.add('body--hidden');
+});
 
-    document.addEventListener('click', e => {
-      e.stopPropagation;
-      if(e.target.classList.contains('page-body')) {
+document.addEventListener('click', (e) => {
+    e.stopPropagation;
+    if (e.target.classList.contains('page-body')) {
         closeMenu();
-      }
-      return;
-    })
+    }
+    return;
+});
 
-    closeBtn.addEventListener('click', closeMenu);
+closeBtn.addEventListener('click', closeMenu);
 
 // active el category list
 
@@ -44,20 +47,38 @@ categoryListItems.forEach((item) => {
 
 // scroll to top
 
-toTopBtn.addEventListener('click', ()=> {
-  scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
+toTopBtn.addEventListener('click', () => {
+    scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
 });
 
-// scrollbar 
+// scrollbar
 
-const scrollbar = function() {
-    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+const scrollbar = function () {
+    let winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    let height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    indicator.style.width = scrolled + "%";
-
-}
+    indicator.style.width = scrolled + '%';
+};
 window.addEventListener('scroll', scrollbar);
+
+// favorite buttons
+favoriteBtns.forEach((btn) => {
+    btn.addEventListener('click', function (e) {
+        const parent = e.target.parentElement.parentElement;
+        if (!parent.classList.contains('btn')) {
+            return;
+        }
+        parent.classList.toggle('active');
+    });
+});
+
+// zavivkakrd.ru/himzavivka-online/obuchenie-himicheskoj-zavivke-volos
+// zavivkakrd.ru/himzavivka-online/kursy-po-biozavivke-volos
+// zavivkakrd.ru/himzavivka-online/karving-volos-obuchenie
+// zavivkakrd.ru/himzavivka-online/prikornevoj-obyom-flisap-obuchenie
