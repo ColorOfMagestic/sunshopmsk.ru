2ed0619a409ff088de8406153eb34ddd19ff2547
Kuznetsov Pavel <colorOfMagic@mail.ru>
2023-01-26 16:44:31 +0500

    Adding anchor links

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -107,3 +107,17 @@ cartClose.addEventListener('click', ()=> closeMenu(cart,'modal--active'));
 selfWrapperInput.addEventListener('focusin', ()=>{
     selfWrapper.classList.add('self');
 });
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const blockID = anchor.getAttribute('href').substr(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
